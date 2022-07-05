6939b18b3e7ada9a4fbee0c153d7423e51563ddf
Turi <40395348+turinaf@users.noreply.github.com>
2022-07-05 22:56:28 +0800

    Sidebar

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Ka
 import './App.css'
 const App = () => {
  
-  const activeMenu = false;
+  const activeMenu = true;
 
   return (
     <div>
