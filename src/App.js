9db0e173b68f52d6e94565aec16398c5cd3679d4
Turi <40395348+turinaf@users.noreply.github.com>
2022-07-06 09:47:52 +0800

    Sidebar toggle

--- src/App.js
+++ src/App.js
@@ -5,12 +5,15 @@ import {TooltipComponent } from '@syncfusion/ej2-react-popups'
 
 import { Navbar, Footer, Sidebar, ThemeSettings} from './components'
 
-import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Area, Bar, Pie, Line, Financial, ColorPicker, ColorMapping, Editor} from './pages'
+import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Area, Bar, Pie, Line, Financial, ColorPicker, ColorMapping, Editor} from './pages';
+
+import { useStateContext } from './contexts/ContextProvider'
 
 import './App.css'
 const App = () => {
  
-  const activeMenu = true;
+  const {activeMenu} = useStateContext();
+  // const activeMenu = true;
 
   return (
     <div>
--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import './index.css'
 import App from './App';
+import { ContextProvider } from "./contexts/ContextProvider";
 
 // ReactDOM.render(<App />, document.getElementById('root'));
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-    <App />
+    <ContextProvider>
+        <App />
+    </ContextProvider>,
+   
 );
